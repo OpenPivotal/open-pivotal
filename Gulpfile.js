6c1dbfd24f3e7972b08d9f4e1bf8fbeaf7cc6ea3
Ben Janecke <benjanecke@gmail.com>
2015-04-09 09:59:30 +0100

    Initial project layout
    * ES6 to js 
    * Sass
    * Bootstrap 
    * Handlebars
    * Backbone 
    * jQuery

---
+++ Gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp')
+  , babel = require('gulp-babel')
+  , concat = require('gulp-concat')
+  , sass = require('gulp-sass')
+  , handlebars = require('gulp-handlebars')
+  , wrap = require('gulp-wrap')
+  , declare = require('gulp-declare')
+  , sourcemaps = require('gulp-sourcemaps')
+  , paths = {
+    scripts: 'src/**/*.js',
+    templates: 'src/templates/*.hbs',
+    tests: 'tests/**/*.js',
+    static: 'static/**',
+    bower: 'bower/**',
+    sass: 'sass/*.scss'
+  };
+
+gulp.task('default', [ 'scripts', 'tests', 'templates', 'copy-static', 'copy-bower', 'sass' ], function () {
+});
+
+gulp.task('watch', [ 'default' ], function () {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.tests, ['tests']);
+  gulp.watch(paths.templates, ['templates']);
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.static, ['copy-static']);
+  gulp.watch(paths.bower, ['copy-bower']);
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(paths.scripts)
+             .pipe(babel({
+                "modules": "amd"
+              }))
+             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('tests', function () {
+  return gulp.src(paths.tests)
+             .pipe(babel({
+                "modules": "amd"
+              }))
+             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('templates', function () {
+  return gulp.src(paths.templates)
+             .pipe(handlebars())
+             .pipe(wrap('Handlebars.template(<%= contents %>)'))
+             .pipe(declare({
+               namespace: 'App.templates',
+               noRedeclare: true, // Avoid duplicate declarations
+             }))
+             .pipe(concat('templates.js'))
+             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.sass)
+             .pipe(sourcemaps.init())
+             .pipe(sass())
+             .pipe(concat('app.css'))
+             .pipe(sourcemaps.write('.'))
+             .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy-static', function () {
+  return gulp.src(paths.static)
+             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-bower', function () {
+  return gulp.src(paths.bower)
+             .pipe(gulp.dest('dist'));
+});
