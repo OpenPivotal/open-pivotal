101daf45f272ad27910fd2ee66c8e591a83c1109
Ben Janecke <benjanecke@gmail.com>
2015-04-16 15:41:56 +0100

    Add fixtures

---
+++ tests/fixtures/cards-done.js
@@ -0,0 +1,25 @@
+export default [{
+    "kind": "story",
+    "id": 12345,
+    "created_at": "2015-04-13T15:21:06Z",
+    "updated_at": "2015-04-13T15:31:58Z",
+    "estimate": 2,
+    "story_type": "feature",
+    "name": "Done Card",
+    "description": "New thing described here!\n\n",
+    "current_state": "unstarted",
+    "requested_by_id": 636721,
+    "project_id": 1,
+    "url": "https://www.pivotaltracker.com/story/show/12345",
+    "owner_ids": [],
+    "labels": [
+        {
+            "id": 8820552,
+            "project_id": 1,
+            "kind": "label",
+            "name": "public",
+            "created_at": "2014-06-27T09:14:47Z",
+            "updated_at": "2014-06-27T09:14:47Z"
+        }
+    ]
+}]
